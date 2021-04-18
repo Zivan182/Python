2f717f374eb355ba60dd11747f754593832c5f58
Иван Строгальщиков <71238076+Zivan182@users.noreply.github.com>
2021-04-19 02:07:26 +0300

    Update wall.py

--- wall.py
+++ wall.py
@@ -1,6 +1,6 @@
 from variables import *
 
-class wall():
+class Wall():
     def __init__(self, canvas, x, y, len, color):
         self.canvas = canvas
         self.x = x
@@ -8,17 +8,25 @@ class wall():
         self.len = len
         self.color = color
 
-class vertical_wall(wall):
+
+    def draw(self):
+        pass
+
+class VerticalWall(Wall):
     def __init__(self, canvas, x, y, len = WALL_LEN, color = WALL_COLOR):
-        wall.__init__(self, canvas, x, y, len, color)
+        Wall.__init__(self, canvas, x, y, len, color)
         self.draw()
+
+
     def draw(self):
         self.canvas.create_line(self.x, self.y, self.x, self.y + self.len, fill=WALL_COLOR)
 
 
-class horizontal_wall(wall):
+class HorizontalWall(Wall):
     def __init__(self, canvas, x, y, len = WALL_LEN, color = WALL_COLOR):
-        wall.__init__(self, canvas, x, y, len, color)
+        Wall.__init__(self, canvas, x, y, len, color)
         self.draw()
+
+
     def draw(self):
         self.canvas.create_line(self.x, self.y, self.x + self.len, self.y, fill=WALL_COLOR)
