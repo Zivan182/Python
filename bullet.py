2014726f2c6f25efe6174c1e1351cbbc52c52500
Иван Строгальщиков <71238076+Zivan182@users.noreply.github.com>
2021-04-19 02:00:12 +0300

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -4,7 +4,7 @@ from geometry import *
 from variables import *
 
 
-class bullet():
+class Bullet():
     def __init__(self, canvas, x, y, dx, dy, r = BULLET_R, color = BULLET_COLOR):
         self.canvas = canvas
         self.x = x
@@ -13,8 +13,10 @@ class bullet():
         self.color = color
         self.dx = dx
         self.dy = dy
-        self.id = self.canvas.create_oval(self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r, fill = BULLET_COLOR)
+        self.id = self.canvas.create_oval(self.x - self.r, self.y - self.r,
+                                          self.x + self.r, self.y + self.r, fill = BULLET_COLOR)
         self.live = True
+        self.stopped = False
         self.canvas.after(10000, self.stop)
         self.move()
 
@@ -27,7 +29,6 @@ class bullet():
                 self.dy = -self.dy
             elif distance(self.x, self.y, v_wall.x, v_wall.y + v_wall.len) <= self.r + 2:
                 self.dy = -self.dy
-
         for h_wall in horizontal_walls:
             if (abs(h_wall.y - self.y) <= self.r + 2) and (h_wall.x <= self.x <= h_wall.x + h_wall.len):
                 self.dy = -self.dy
@@ -43,7 +44,7 @@ class bullet():
 
 
     def move(self):
-        if self.live:
+        if self.live and not self.stopped:
             self.collision()
             self.canvas.move(self.id, self.dx, self.dy)
             self.x += self.dx
