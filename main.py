2ba04a46d50729206fd66207e5c93d1931e79f0d
Иван Строгальщиков <71238076+Zivan182@users.noreply.github.com>
2021-04-22 21:14:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from bullet import *
 from generate_maze import *
 from draw_maze import *
 from geometry import *
-from variables import *
+from constants import *
 
 root = Tk()
 root.geometry("1200x600+30+20")
@@ -19,6 +19,25 @@ canvas.quit()
 root.update()
 
 def main():
+    """запускает игру:
+
+    создает границы поля
+
+    up, down : HorizontalWall
+        верхняя и нижняя границы поля
+    left, right : VerticalWall
+        левая и правая границы поля
+
+    запускает draw_maze() для отрисовки лабиринта
+
+    создает танки
+
+    tank_first : Tank1
+        первый танк, находится в левом правом углу поля
+    tank_second : Tank2
+        второй танк, находится в правом нижнем углу поля
+    """
+
     up = HorizontalWall(canvas, 0, 0, WIDTH)
     down = HorizontalWall(canvas, 0, HEIGHT, WIDTH)
     left = VerticalWall(canvas, 0, 0, HEIGHT)
@@ -28,10 +47,8 @@ def main():
     horizontal_walls.append(up)
     horizontal_walls.append(down)
     draw_maze(canvas)
-    tank_second = Tank2(canvas, [[1175,565],[1195,565],[1195,595],[1175,595]])
     tank_first = Tank1(canvas, [[25,35], [5,35], [5,5], [25,5]])
-    tank_first.start()
-    tank_second.start()
+    tank_second = Tank2(canvas, [[1175,565],[1195,565],[1195,595],[1175,595]])
     root.update_idletasks()
     root.update()
     time.sleep(0.01)
@@ -39,3 +56,18 @@ def main():
 
 if __name__=='__main__':
     main()
+
+#pauseButton = Button(root, text="Pause", command=stop)
+#startButton = Button(root, text="Start", command=start)
+#pauseButton.place(x=0, y=0)
+#startButton.place(x=50, y=0)
+#quitButton.pack(expand = True, fill = BOTH)
+#canvas.create_text(10, 10, text='1234', font=('Courier', 15), fill='black')
+
+
+
+
+
+
+
+
